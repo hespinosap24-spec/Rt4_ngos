4a1ff42abedd7dff51a76dad21fbfbaa3ef390ac
hespinosap24-spec <hespinosap24@fpsanturtzilh.eus>
2026-02-10 09:58:01 +0100

    Simplify directory move commands in glpi.sh

--- glpi.sh
+++ glpi.sh
@@ -40,8 +40,8 @@ if (file_exists(GLPI_CONFIG_DIR . '/local_define.php')) {
 EOF
 
 # Mover directorios originales a sus nuevas ubicaciones
-mv /var/www/html/glpi/config/* /etc/glpi/
-mv /var/www/html/glpi/files/* /var/lib/glpi/
+mv /var/www/html/glpi/config/ /etc/glpi/
+mv /var/www/html/glpi/files/ /var/lib/glpi/
 # Nota: El log se mueve de forma específica
 mv /var/lib/glpi/_log /var/log/glpi || true
 
